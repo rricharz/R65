61b8c83e31ad9791c352e114526ee3ec9f2111b1
Rene Richarz <rene.richarz@bluewin.ch>
2019-01-25 12:39:04 +0100

    LED control removed

--- R65-Emulator/Tools/replica-shutdown/pi-shutdown.c
+++ R65-Emulator/Tools/replica-shutdown/pi-shutdown.c
@@ -12,7 +12,7 @@
 #include <stdio.h>
 
 #define PIN 29       // pin 40, wiring pi 29
-#define LED 26       // pin 32, wiring pi 26
+//#define LED 26       // pin 32, wiring pi 26
 
 int ledState = 0;
 
@@ -25,25 +25,25 @@ int main (int argc, char **argv)
 	wiringPiSetup();
 	pinMode(PIN, INPUT);
 	pullUpDnControl (PIN, PUD_UP);
-         pinMode(LED, OUTPUT),
-         digitalWrite(LED, 1);
+         // pinMode(LED, OUTPUT),
+         // digitalWrite(LED, 1);
 
 	do {
 		if (digitalRead(PIN) == 0) {
 			printf("Shutting down...\n");
                         delay(1000);
                         if (digitalRead(PIN) == 0) {
-                                 digitalWrite(LED, 1);
+                                // digitalWrite(LED, 1);
                                 system("/home/pi/bin/max7219 'PI OFF'");
                                 delay(100);
                                 system("shutdown -h now");
                                 exit(0);
                         }
 		}
-                else {
-                        ledState = !ledState;
-                         digitalWrite(LED, ledState);
-                }
+                // else {
+                //        ledState = !ledState;
+                //         digitalWrite(LED, ledState);
+                // }
 		delay(1000);
 	}
 	while (1);
