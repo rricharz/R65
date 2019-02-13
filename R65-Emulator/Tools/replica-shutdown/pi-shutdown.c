55b9275adc5fa61c2c9fee4f151d7eec7de65119
Rene Richarz <rene.richarz@bluewin.ch>
2019-02-13 09:04:13 +0100

    cpu usage display on replica added

--- R65-Emulator/Tools/replica-shutdown/pi-shutdown.c
+++ R65-Emulator/Tools/replica-shutdown/pi-shutdown.c
@@ -1,28 +1,81 @@
 //
 // pi-shutdown.c
-// version for R65 Replica
-// with LED heartbeat
+// Version for R65 Replica
+//
+// Without LED heartbeat
+//   LED heartbeat is handled by the kernel
+//   put the following line at the end of
+//   /boot/config.txt:
+//   dtoverlay=pi3-act-led,gpio=12
+//   where 12 is the gpio of the external led
+//
+// Displays CPU load and temperature on
+// segment display of R65 Replica,
+// if SWITCH is on. Turn SWITCH off when
+// running R65 emulator to see R65 stack
+// pointer and free Pascal memory
 // 
-// Copyright 2017,2018  rricharz 
+// 
+// Copyright 2017,2018,2019  rricharz
+//
+// cpu usage code from Matheus (https://github.com/hc0d3r)
 
 
 #include <wiringPi.h>
-#include <stdlib.h>
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define PIN 29       // pin 40, wiring pi 29
 #define SWITCH 5     // pin 18, wiring pi 5
-//#define LED 26       // pin 32, wiring pi 26
 
 int ledState = 0;
 
 FILE *temperatureFile;
+     
+int switchIsSet;
 
 double T;
 
+int *parser_result(const char *buf, int size){
+	static int ret[10];
+	int i, j = 0, start = 0;
+
+	for(i=0; i<size; i++){
+		char c = buf[i];
+		if(c >= '0' && c <= '9'){
+			if(!start){
+				start = 1;
+				ret[j] = c-'0';
+			} else {
+				ret[j] *= 10;
+				ret[j] += c-'0';
+			}
+		} else if(c == '\n'){
+			break;
+		} else {
+			if(start){
+				j++;
+				start = 0;
+			}
+		}
+	}
+
+	return ret;
+}
+
+
+
 int main (int argc, char **argv)
 {
+        char buf[256];
+	int size, fd, *nums, prev_idle = 0, prev_total = 0, idle, total, i;
+        double usage;
+        
+	fd = open("/proc/stat", O_RDONLY);
 
 	printf("Monitoring wiringPi pin %i for shutdown\n", PIN);
         system("/home/pi/bin/max7219 'pi-65'");
@@ -32,8 +85,8 @@ int main (int argc, char **argv)
 	pullUpDnControl (PIN, PUD_UP);
         pinMode(SWITCH, INPUT);
 	pullUpDnControl (SWITCH, PUD_UP);
-         // pinMode(LED, OUTPUT);
-         // digitalWrite(LED, 1);
+        
+        switchIsSet = 0;
 
 	do {
 		if (digitalRead(PIN) == 0) {
@@ -49,24 +102,66 @@ int main (int argc, char **argv)
 		}
                 else if (digitalRead(SWITCH) != 0){
                     ledState = !ledState;
-                    //         digitalWrite(LED, ledState)
+                    switchIsSet = 1;
+                    
+                    size = read(fd, buf, sizeof(buf));
+                    if(size > 0) {
+
+                        nums = parser_result(buf, size);
+
+		        idle=nums[3];
+
+                        for(i=0, total=0; i<10; i++){
+			    total += nums[i];
+		        }
+
+
+		        int diff_idle = idle-prev_idle;
+		        int diff_total = total-prev_total;
+		        usage = (double)(((double)(1000*(diff_total-diff_idle))/(double)diff_total+5)/(double)10);
+                        printf("\r%%%6.2f", usage);
+		        fflush(stdout);
+                    }
+                    else {
+                        usage = 0.0;
+                    }
+                    
+                    if (usage < 0.0) usage = 0.0;
+                    if (usage > 99.0) usage = 99.0;
+
+		    prev_total = total;
+		    prev_idle = idle;
+
+		    lseek(fd, 0, SEEK_SET);
+                    
                     temperatureFile = fopen(
                         "/sys/class/thermal/thermal_zone0/temp", "r");
                     if (temperatureFile != NULL) {
                         fscanf(temperatureFile, "%lf", &T);
+                        if (T < 0.0) T = 0.0;
+                        if (T > 99999.0) T = 99999.0;
                         T = T / 1000.0;
-                        char s[64];
-                        if (ledState)
-                            sprintf(s,"/home/pi/bin/max7219 'Pi-65 %2.0f'", T);
-                        else
-                            sprintf(s,"/home/pi/bin/max7219 'Pi 65 %2.0f'", T);
-                        printf("%s\n",s);
-                        system(s);
+                        fclose (temperatureFile);
                     }
-                    fclose (temperatureFile);
-                }
+                    else T = 0.0;
                 
-		delay(500);
+                    char s[64];
+                    if (ledState)
+                        sprintf(s,"/home/pi/bin/max7219 'PI %02.0f %02.0f'",
+                            usage, T);
+                    else
+                        sprintf(s,"/home/pi/bin/max7219 'PI %02.0f-%02.0f'",
+                            usage, T);
+                    printf("%s\n",s);
+                    system(s);
+                
+                }
+                else if (switchIsSet) {
+                    switchIsSet = 0;
+                    system ("/home/pi/bin/max7219 '        '");
+                        
+                }              
+		delay(1000);
 	}
 	while (1);
 }
